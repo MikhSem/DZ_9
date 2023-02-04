af6256f3f692f60999543e0b359aa717598d084f
Mikhail <cdr15@rambler.ru>
2023-02-04 20:05:17 +0300

    Задачи 1,2  решены

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 64: Выполнить с помощью рекурсии.Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+/*
+Console.Write("Введи число: ");
+int N  = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N,1));
+
+// ------------------Метод------------------
+string PrintNumbers(int start, int end){
+    if(start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start - 1, end));
+} 
+*/
+
+// Задача 66: Выполнить с помощью рекурсии.Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите певрое (минимальное) число диапазона");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите втророе (масимальное) число диапазона");
+
+int N = int.Parse(Console.ReadLine()!);
+
+if (M > N)
+    Console.WriteLine($"Первое число {M} не меньше второго {N}, попробуйте снова");
+else
+{
+    Console.WriteLine($"Сумма чисел от {M} до {N}  равна {SummOf(N)}");
+}
+
+
+int SummOf(int N)
+{
+    if (M == N) return M;
+    return (N + (SummOf(N - 1)));
+}
